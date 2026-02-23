1355606faa12e5641dcb6e0da7c3b935ac21a4b5
Vanessa Triviño <trivinomarinv@gmail.com>
2026-02-23 12:19:43 -0500

    se actualizaron archivos

--- Consumo de API/index.js
+++ Consumo de API/index.js
@@ -1,29 +1,44 @@
 const API_URL = 'https://rickandmortyapi.com/api/character';    
 const container = document.getElementById('container-character'); 
+const btn = document.getElementById('fetch-btn');
 
-async function fetchCharacters() { // Función asíncrona para obtener los personajes
+btn.addEventListener('click', () => {
+    fetchCharacters();
+});
+
+async function fetchCharacters() { 
+    btn.innerText = "Cargando...";
+    btn.disabled = true;
+
     try {
-        const response = await fetch(API_URL);  // Realiza la solicitud a la API
-        if (!response.ok) {  // Verifica si la respuesta es exitosa
+        const response = await fetch(API_URL);  
+        
+        if (!response.ok) {  
             throw new Error(`HTTP error! status: ${response.status}`); 
         }
+
         const data = await response.json(); 
         renderCards(data.results);  
+
+        btn.innerText = "Actualizar Personajes";
+
     } catch (error) {
         console.error("Error al obtener los datos:", error);
         container.innerHTML = `<p>Hubo un error al cargar los personajes.</p>`;
+        btn.innerText = "Reintentar consulta";
+    } finally {
+        // finally se ejecuta siempre, haya error o no, para reactivar el botón
+        btn.disabled = false;
     }
 }
 
-
-function renderCards(characters) {  // Recibe un array de personajes
-    container.innerHTML = ''; // Limpiar contenedor
+function renderCards(characters) { 
+    container.innerHTML = ''; // Limpiar contenedor para no duplicar datos
 
     characters.forEach(character => {
         const card = document.createElement('div');
         card.classList.add('card');
 
-// Agregar contenido a cada cuadro
         card.innerHTML = `      
             <img src="${character.image}" alt="${character.name}">
             <div class="card-info">
@@ -33,9 +48,8 @@ function renderCards(characters) {  // Recibe un array de personajes
                 <p class="location-name">${character.location.name}</p>
             </div>  
         `;  
-        // Agregar el cuadro al contenedor
+        
         container.appendChild(card);
     });
 }
-    // Llamar a la función para iniciar el proceso
-fetchCharacters();
+
